64519a98177caf4a16cd6d9e8ee9f6824bbc576e
thomas.pink <thomas.pink@netural.com>
2015-09-09 22:20:08 +0200

    es6 + observers + computed properties

--- test/test-controller.js
+++
@@ -1,7 +0,0 @@
-describe('Controller: ', function() {
-    var jQuery = require('../bower_components/jquery/dist/jquery.js');
-    var Mojito = require('../lib/core.js');
-    it('mojito`s default controller has been created', function() {
-            //Mojito.should.have.property('Controller')
-    });
-});
