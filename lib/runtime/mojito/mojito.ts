8209a6c9ff71c36f443dc30d3d220c11c936bad9
Thomas Pink <thomas.pink@netural.com>
2016-03-18 09:32:34 +0100

    singleton application

--- lib/runtime/mojito/mojito.ts
+++ lib/runtime/mojito/mojito.ts
@@ -2,41 +2,48 @@ import { Meta, get, set } from '../../core';
 import { Application, singleton } from '../../runtime';
 import { assert } from '../../debug';
 
-
-@singleton
 export class Mojito {
     
     private static GLOBAL_NAMESPACE = 'Mojito';
-    
+
     constructor() {
-        
-        let instance:Mojito = get(self, Mojito.GLOBAL_NAMESPACE);
+        let instance: Mojito = get(self, Mojito.GLOBAL_NAMESPACE);
         if (!instance) {
             instance = this;
             set(self, Mojito.GLOBAL_NAMESPACE, instance);
         }
-        
     }
-    
+
     registerApplication(application: Application): Application {
         assert(arguments.length === 1, 'registerApplication must be called with one argument: an application instance');
         assert(application instanceof Application, 'The argument provided to the registerApplication method must be an application instance', TypeError);
-        
+
         return Meta.peek(this).setProperty('applications', application.name, application);
     }
-    
+
     getApplication(applicationName: string): Application {
         assert(arguments.length === 1, 'getApplication must be called with one argument: an application name');
         assert(typeof applicationName === 'string', 'The applicationName provided to the getApplication method must be a string', TypeError);
-        
+
         return Meta.peek(this).getProperty('applications', applicationName);
     }
-    
+
     getApplications() {
-       return Meta.peek(this).getMember('applications');
+        return Meta.peek(this).getMember('applications');
     }
-    
-    static getInstance():Mojito {
+
+    static getInstance(): Mojito {
+        debugger;
+        if (!get(Mojito, '_instance')) {
+            
+            let instance = new Mojito();
+            Object.defineProperty(Mojito, '_instance', {
+                writable: false,
+                configurable: false,
+                enumerable: false,
+                value: instance
+            });
+        }
         return get(Mojito, 'instance');
     }
 }
