666024ed8b54a65dbaa07ae7959559425c521f67
Dominik Strasser <dominik.strasser@netural.com>
2016-05-07 18:15:14 +0200

    fixed barrels #2

---
+++ lib/core/core.ts
@@ -0,0 +1,8 @@
+export { get } from './get/get';
+export { set } from './set/set';
+export { Meta } from './meta/meta';
+export { CoreObject } from './object/object';
+export { CoreArray } from './array/array';
+export { CoreClass } from './class/class';
+export { CoreView, onDidAttachView, onDidRenderView } from './view/view';
+export { Observer, observes } from './observer/observer';
