3ac9fbe20576e6ae897edbe249166455407b1e3a
Thomas Pink <thomas.pink@netural.com>
2016-05-07 14:44:25 +0200

    fixed imports

--- examples/example_01.ts
+++ examples/example_01.ts
@@ -1,17 +1,23 @@
-import {assert} from 'debug/assert/assert';
-import { CoreObject, set, get } from 'core';
-import { Application } from 'runtime';
+// import { assert } from 'mojito/debug';
+import {assert} from '../lib/debug';
+
+// import { CoreObject, set, get } from 'mojito/core';
+import { CoreObject, set, get } from '../lib/core';
+
+// import { Application } from 'mojito/runtime';
+import { Application } from '../lib/runtime';
+
 assert(true, 'test');
 
 //var app = new Application('xx'); 
 
-var x = new CoreObject({});
-x.set('y', 'test');
-console.log(x);
-class X{
-  constructor () {
-    console.log('XXX');
-  }
-}
+// var x = new CoreObject({});
+// x.set('y', 'test');
+// console.log(x);
+// class X{
+//   constructor () {
+//     console.log('XXX');
+//   }
+// }
 
-new X();
+// new X();
