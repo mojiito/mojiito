81d34ef5d34103a4f8fec64db8aff95870045949
Thomas Pink <thomas.pink@netural.com>
2016-05-08 11:39:29 +0200

    removed lib/mojito.ts

--- lib/mojito.ts
+++
@@ -1,8 +0,0 @@
-import { CoreObject, CoreArray, CoreClass, CoreView, Meta } from './core';
-import { Mojito, Application, Controller, Service, singleton, inject, injectable, register } from './runtime';
-import { assert } from './debug';
-
-@register({})
-class App extends Application {
-    
-}
