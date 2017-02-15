a2f92880e06157548e2c4983978b531cb2f9aba4
Thomas Pink <thomas.pink@me.com>
2017-02-15 17:06:50 +0100

    added npm publish

---
+++ scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+PACKAGES=(core
+  platform-browser)
+
+for PACKAGE in ${PACKAGES[@]}
+do
+  echo "====== ${bold}PUBLISHING to NPM: ${green}${PACKAGE}${normal} ====="
+  DESTDIR=${PWD}/dist/packages-dist/${PACKAGE}
+  cd $DESTDIR
+  npm publish
+done
