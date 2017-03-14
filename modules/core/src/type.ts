117f6bdb77d16a569dfab45182055162673c84a3
Thomas Pink <thomas.pink@me.com>
2017-03-14 13:06:09 +0100

    type formatted

--- modules/core/src/type.ts
+++ modules/core/src/type.ts
@@ -1,10 +1,10 @@
 export interface ClassType<T> extends Function {
-    new (...args: Array<any>): T;
-    constructor: Function | any[];
-    [propertyName: string]: any;
-    name: string;
+  new (...args: Array<any>): T;
+  constructor: Function | any[];
+  [propertyName: string]: any;
+  name: string;
 }
 
 export function isClassInstance(instance: any): boolean {
-    return typeof instance === 'object' && !!instance['constructor'];
+  return typeof instance === 'object' && !!instance['constructor'];
 }
